7c5624aa7ec36f711a805b1dfb81b4029be5b65b
timFox <34039148+tk427@users.noreply.github.com>
2023-03-17 08:21:54 -0400

    Delete Get-QTOD.ps1

--- Get-QTOD.ps1
+++
@@ -1,6 +0,0 @@
-Function Get-QOTD
-{
-    $color = @('Gray','Red', 'Yellow', 'Green', 'Cyan', 'Magenta', 'DarkYellow', 'DarkGray')
-    $QTOD = Get-Content "C:\Users\gtfox\OneDrive - columbusairports.com\Documents\GitHub\GTFox\Powershell\QTOD.txt" | get-random
-    Write-host `n$QTOD -ForegroundColor (Get-random $color)
-}
